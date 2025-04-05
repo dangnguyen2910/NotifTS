3b1676eebfe54b244c953487c86a0a0765acf76e
Nguyen Hai Dang <dangnguyen29124@gmail.com>
2025-04-05 15:44:26 +0700

    First commit

---
+++ app/src/main/java/usth/intern/notifts/MainActivity.kt
@@ -0,0 +1,44 @@
+package usth.intern.notifts
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import usth.intern.notifts.ui.theme.NotifTSTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NotifTSTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                        NotifTSApp()
+                }
+            }
+        }
+
+    }
+}
+
+@Composable
+fun NotifTSApp(modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+            .fillMaxSize()
+    ) {
+        Text(
+            text = "Hello"
+        )
+    }
+}
