0b071d06f60ec556810af5b0040e2a2c9ae7fdd2
Nguyen Hai Dang <dangnguyen29124@gmail.com>
2025-04-30 08:40:15 +0700

    optim: Finish activity on pressing home

--- app/src/main/java/usth/intern/notifts/MainActivity.kt
+++ app/src/main/java/usth/intern/notifts/MainActivity.kt
@@ -41,4 +41,9 @@ class MainActivity : ComponentActivity() {
             startActivity(intent)
         }
     }
+
+    override fun onUserLeaveHint() {
+        super.onUserLeaveHint()
+        finish()
+    }
 }
