bda00a8265d2188bc366398fd20874ba89c2b614
Nguyen Hai Dang <dangnguyen29124@gmail.com>
2025-05-03 10:54:37 +0700

    ui: Change SearchBar to OutlinedTextField for customization

--- app/src/main/java/usth/intern/notifts/ui/ManagerScreen.kt
+++ app/src/main/java/usth/intern/notifts/ui/ManagerScreen.kt
@@ -1,6 +1,8 @@
 package usth.intern.notifts.ui
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -8,28 +10,41 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import usth.intern.notifts.R
 import usth.intern.notifts.data.db.Notification
 
 @Composable
@@ -61,13 +76,46 @@ fun ManagerContent(
     modifier: Modifier = Modifier,
 ) {
     Column (modifier = modifier) {
-        KeywordsSearchBar(
-            query = query,
-            onQueryChange = onTypingSearch,
-            onSearch = onEnterSearch,
-            searchResults = listOf(),
-            onResultClick = { _: String -> }
-        )
+        Row(modifier = Modifier.fillMaxWidth()) {
+            KeywordsSearchBar(
+                query = query,
+                onQueryChange = onTypingSearch,
+                onSearch = onEnterSearch,
+//            searchResults = listOf(),
+//            onResultClick = { _: String -> }
+            )
+
+//            Icon(
+//                imageVector = Icons.Default.Delete,
+//                contentDescription = "Filter",
+//                modifier = modifier
+//                    .size(30.dp)
+//            )
+            Column (
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+//                modifier = Modifier.padding(top = 8.dp)
+            ) {
+                IconButton(
+                    onClick = {/*todo*/},
+//                    colors = ButtonDefaults.buttonColors(
+//                        containerColor = Color(0xFFCCC2DC)
+//                    ),
+                    modifier = Modifier
+                        .height(55.dp)
+                        .background(MaterialTheme.colorScheme.tertiaryContainer)
+                        .clip(RoundedCornerShape(40.dp))
+
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.filter_icon),
+                        contentDescription = "Favorite",
+                        modifier = Modifier
+                            .size(35.dp)
+                    )
+                }
+            }
+        }
 
         Button(
             onClick = onReload,
@@ -85,8 +133,8 @@ fun KeywordsSearchBar(
     query: String,
     onQueryChange: (String) -> Unit,
     onSearch: (String) -> Unit,
-    searchResults: List<String>,
-    onResultClick: (String) -> Unit,
+//    searchResults: List<String>,
+//    onResultClick: (String) -> Unit,
 //     Customization options
     modifier: Modifier = Modifier,
     placeholder: @Composable () -> Unit = { Text("Search") },
@@ -96,37 +144,31 @@ fun KeywordsSearchBar(
 //    leadingContent: (@Composable () -> Unit)? = null,
 ) {
 //    var expanded by rememberSaveable { mutableStateOf(false) }
-    val expanded = false
 
-    Column(
-        modifier = modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        SearchBar(
-            inputField = {
-                SearchBarDefaults.InputField(
-                    query = query,
-                    onQueryChange = onQueryChange,
-                    onSearch = {
-                        onSearch(query)
-                        //                    expanded = false
-                    },
-                    expanded = expanded,
-                    onExpandedChange = {/*todo*/ },
-                    placeholder = placeholder,
-                    leadingIcon = leadingIcon,
-                    trailingIcon = trailingIcon,
-                )
-            },
-            expanded = expanded,
-            onExpandedChange = {/*todo*/ },
-            shape = RoundedCornerShape(8.dp),
-            modifier = modifier
-                .fillMaxWidth(0.98f)
-        ) {
+//    Column(
+//        horizontalAlignment = Alignment.CenterHorizontally,
+//        modifier = modifier.padding(start = 8.dp)
+//    ) {
+    val keyboardController = LocalSoftwareKeyboardController.current
 
-        }
-    }
+    OutlinedTextField(
+        value = query,
+        onValueChange = onQueryChange,
+        keyboardOptions = KeyboardOptions.Default.copy(
+            imeAction = ImeAction.Search
+        ),
+        keyboardActions = KeyboardActions(
+            onSearch = {
+                onSearch(query)
+                keyboardController?.hide()
+            }
+        ),
+        shape = RoundedCornerShape(10.dp),
+        leadingIcon = leadingIcon,
+        placeholder = placeholder,
+        modifier = modifier.padding(start = 8.dp, end = 8.dp).fillMaxWidth(0.85f)
+        )
+//    }
 }
 
 @Composable
