86bdd6ec6d2349cbf31d19f3489fe713bc60ab38
Nguyen Hai Dang <dangnguyen29124@gmail.com>
2025-06-05 20:23:42 +0700

    feat: make button to show tts settings screen

--- app/src/main/java/usth/intern/notifts/MainActivity.kt
+++ app/src/main/java/usth/intern/notifts/MainActivity.kt
@@ -1,29 +1,37 @@
 package usth.intern.notifts
 
 import android.app.NotificationManager
+import android.content.ActivityNotFoundException
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import usth.intern.notifts.domain.NotificationListener
+import usth.intern.notifts.ui.settings.SettingsViewModel
 import usth.intern.notifts.ui.theme.NotiftsTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val settingsViewModel: SettingsViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             NotiftsTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
-                    NotiftsScreen()
+                    NotiftsScreen(settingsViewModel)
                 }
             }
 
@@ -39,6 +47,16 @@ class MainActivity : ComponentActivity() {
         if (!notificationIsAccessed) {
             startActivity(intent)
         }
+
+        settingsViewModel.openTtsSetting.observe(this) {
+            try {
+                val ttsSettingsIntent = Intent("com.android.settings.TTS_SETTINGS")
+                startActivity(ttsSettingsIntent)
+            } catch (e: ActivityNotFoundException) {
+                Log.e("MainActivity", "TTS settings not found")
+            }
+        }
+
     }
 
 }
